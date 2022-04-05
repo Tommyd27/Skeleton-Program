19e7814ad9d316f12bfa8090ff93448c23b69d9e
Tommy <tom@smart-homes.com>
2022-04-05 14:07:43 +0100

    Error with Grid Bounds and Addition?

--- Python Skeleton Program original.py
+++ Python Skeleton Program original.py
@@ -3,8 +3,13 @@ import random as r
 
 def sign(num):
 	return -1 if num < 0 else 1
+class GameOverException(Exception):
+	def __init__(s, winLoss):
+		s.winLoss = winLoss
+		super().__init__()
 
-
+class Exit(Exception):
+	pass
 class Position:  # Anything on the map will be a position
 	def __init__(s, x, y, gridIdentifier=1) -> None:
 		s._x = x
@@ -17,11 +22,17 @@ class Position:  # Anything on the map will be a position
 	def __add__(s, o) -> bool:
 		return s._x + o.x and s._y + o.y
 
+	def __le__(s, o) -> bool:
+		return s._x <= o.x and s._y <= o.y
+
 	def __lt__(s, o) -> bool:
 		return s._x < o.x and s._y < o.y
 
 	def __gt__(s, o) -> bool:
-		return s._x > o.x and s._y > o.y
+		return s._x > o.x and s._x > o.y
+
+	def __ge__(s, o) -> bool:
+		return s._x >= o.x and s._y >= o.y
 
 	def __add__(s, o):
 		return Position(s._x + o.x, s._y + o.y)
@@ -61,21 +72,26 @@ class Position:  # Anything on the map will be a position
 
 
 class Item(Position):
-	def __init__(s, x, y, identifier):
-		super(Item, s).__init__(x, y, identifier)
+	def __init__(s, pos, identifier):
+		super(Item, s).__init__(pos[0], pos[1], identifier)
 
 
 class Character(Position):
-	possibleMoves = {"w" : Position(1, 0),
-					 "a" : Position(0, 1),
-					 "s" : Position(-1, 0),
-					 "d" : Position(0, -1),
+	possibleMoves = {"w" : Position(0, -1),
+					 "a" : Position(1, 0),
+					 "s" : Position(0, 1),
+					 "d" : Position(-1, 0),
 					}
-	def __init__(s, x=0, y=0):
-		super(Position, s).__init__(x, y, "*")
-
+	def __init__(s, pos):
+		super(Character, s).__init__(pos[0], pos[1], "*")
+		s._collectedItems = []
+	@property
+	def collectedItems(s):
+		return s._collectedItems
+	
 	def MakeMove(s, grid):
 		while True:
+			print("Press Control C at anytime to Exit to Menu")
 			userMoveInput = input("Where would you like to move (WASD): ").lower()
 			try:
 				move = Character.possibleMoves[userMoveInput]
@@ -86,15 +102,22 @@ class Character(Position):
 			except KeyError:
 				pass
 			print("Sorry, that is not a valid move.")
-
+	def TakeTurn(s, grid):
+		s.MakeMove(grid)
+		for pos in grid:
+			if isinstance(pos, Item):
+				if pos == s:
+					s._collectedItems.append(pos)
 class Enemy(Position):
 	positionIdentifiers = {True  : "!",
 						   False : " ",	
 						  }
-	def __init__(s, x=0, y=0, isAwake=False):
-		super(Position, s).__init__(x, y)
+	defaultSleepCounter = 4
+	def __init__(s, pos, isAwake=False, sleepCounter = -1):
+		super(Enemy, s).__init__(pos[0], pos[1])
 		s._awake = isAwake
-		s.gID = Enemy.positionIdentifiers[s._awake]
+		s._sleepCounter = sleepCounter
+		s._gID = Enemy.positionIdentifiers[s._awake]
 
 	@property
 	def awake(s):
@@ -105,6 +128,26 @@ class Enemy(Position):
 		s._awake = isAwake
 		s.gID = Enemy.positionIdentifiers[s._awake]
 
+	def toggleAwake(s):
+		s._awake = not s._awake
+	@property
+	def sleepCounter(s):
+		return s._sleepCounter
+	@sleepCounter.setter
+	def sleepCounter(s, count):
+		s._sleepCounter = count
+
+
+	def TakeTurn(s, player):
+		s._sleepCounter -= 1
+		if s._sleepCounter == 0:
+			s.toggleAwake()
+		if s._awake:
+			if s == player:
+				raise GameOverException(0)
+			s.MakeMove(player)
+			if s == player:
+				raise GameOverException(0)
 	def MakeMove(s, Player: Character):
 		# Calculate distances
 		idealMove = [("x", Player.x - s._x), ("y", Player.y - s._y)]
@@ -121,8 +164,8 @@ class Enemy(Position):
 
 
 class Trap(Item):
-	def __init__(s):
-		Item.__init__(s)
+	def __init__(s, pos):
+		Item.__init__(s, pos)
 		s._triggered = False
 
 	@property
@@ -150,10 +193,20 @@ class Grid:
 	def gridState(s):
 		return s._gridState
 
+	def __iter__(s):
+		s.iC = 0
+		return s
+	def __next__(s):
+		if s.iC < len(s._gridState):
+			output = s._gridState[s.iC]
+			s.iC += 1 
+			return output
+		else:
+			raise StopIteration
 	def __repr__(s) -> str:
-		dividerLine = ["-" * (s._posMax.x * 2 + 1)]
-		output = dividerLine
-		for y in range(s._posMax):
+		dividerLine = "-" * (s._posMax.x * 2 + 1)
+		output = [dividerLine]
+		for y in range(s._posMax.y):
 			line = "|"
 			for x in range(s._posMax.x):
 				try:
@@ -162,7 +215,9 @@ class Grid:
 					positionIdentifier = Grid.blankSpace
 				line += f"{positionIdentifier}|"
 			output.append(line)
-		return output
+			output.append(dividerLine)
+		outputSTR = "\n".join(output)
+		return outputSTR
 
 	def AddPositionsToGrid(s, *args, unique = False):
 		lists = [x for x in args if isinstance(x, list)]
@@ -177,7 +232,7 @@ class Grid:
 				raise ValueError("Item Already at that Location")
 			s._gridState.append(positionToAdd)
 	def InGrid(s, position):
-		return s._posMin <= position <= s._posMax
+		return s._posMin < position <= s._posMax
 	def GenerateRandomPosition(s, unique = True):
 		x = r.randint(0, s._posMax.x - 1)
 		y = r.randint(0, s._posMax.y - 1)
@@ -190,121 +245,60 @@ class Grid:
 
 class Game:
 	xSize = 10
-	ySize = 10
+	ySize = 6
 
-	def __init__(s, setPosition = False):
-		s.Cave = Grid()
+	def Start(s, setPosition = False):
+		print(f"Set Positions: {setPosition}")
+		s.Cave = Grid(Game.xSize, Game.ySize)
 		if setPosition:
-			s.Player = Character(0, 0)
-			s.Monster = Enemy(5, 5)
-			s.items = [Item(3, 3, "F"), Item(6, 2, "T"), Item(8, 8, "P")]
+			s.Player = Character((0, 0))
+			s.Monster = Enemy((5, 5))
+			s.items = [Item((3, 3), "F"), Item((6, 2), "T"), Item((8, 8), "P")]
 		else:
 			s.Player = Character(s.Cave.GenerateRandomPosition(False).pos)
-			s.Monster = Character(s.Cave.GenerateRandomPosition().pos)
+			s.Monster = Enemy(s.Cave.GenerateRandomPosition().pos, sleepCounter = 2)
 			itemIdentifiers = ["F", "T", "P"]
 			r.shuffle(itemIdentifiers)
 			s.items = [Item(s.Cave.GenerateRandomPosition().pos, itemIdentifiers.pop(0)) for _ in range(r.randint(1, len(itemIdentifiers)))]
 		s.Cave.AddPositionsToGrid(s.Player, s.Monster, s.items)
+		s.Play()
 		r.seed()
 
-	def Play(self):
-		self.Cavern.Display(self.Monster.GetAwake())
-		while not Eaten and not FlaskFound and (MoveDirection != 'M'):
-			ValidMove = False
-			while not ValidMove:
-				self.DisplayMoveOptions()
-				MoveDirection = self.GetMove()
-				ValidMove = self.CheckValidMove(MoveDirection)
-			if MoveDirection != 'M':
-				self.Cavern.PlaceItem(self.Player.GetPosition(), ' ')
-				self.Player.MakeMove(MoveDirection)
-				self.Cavern.PlaceItem(self.Player.GetPosition(), '*')
-				self.Cavern.Display(self.Monster.GetAwake())
-				FlaskFound = self.Player.CheckIfSameCell(
-					self.Flask.GetPosition())
-				if FlaskFound:
-					self.DisplayWonGameMessage()
-				Eaten = self.Monster.CheckIfSameCell(self.Player.GetPosition())
-				# This selection structure checks to see if the player has triggered one of the traps in the cavern
-				if not self.Monster.GetAwake() and not FlaskFound and not Eaten and (self.Player.CheckIfSameCell(self.Trap1.GetPosition()) and not self.Trap1.GetTriggered() or self.Player.CheckIfSameCell(self.Trap2.GetPosition()) and not self.Trap2.GetTriggered()):
-					self.Monster.ChangeSleepStatus()
-					self.DisplayTrapMessage()
-					self.Cavern.Display(self.Monster.GetAwake())
-				if (self.Monster.GetAwake()) and not Eaten and not FlaskFound:
-					Count = 0
-					while Count != 2 and not Eaten:
-						self.Cavern.PlaceItem(self.Monster.GetPosition(), ' ')
-						Position = self.Monster.GetPosition()
-						self.Monster.MakeMove(self.Player.GetPosition())
-						self.Cavern.PlaceItem(self.Monster.GetPosition(), 'M')
-						if self.Monster.CheckIfSameCell(self.Flask.GetPosition()):
-							self.Flask.SetPosition(Position)
-							self.Cavern.PlaceItem(Position, 'F')
-						Eaten = self.Monster.CheckIfSameCell(
-							self.Player.GetPosition())
-						print()
-						print('Press Enter key to continue')
-						input()
-						self.Cavern.Display(self.Monster.GetAwake())
-						Count = Count + 1
-			if Eaten:
-				self.DisplayLostGameMessage()
-
-	def DisplayMoveOptions(self):
-		print()
-		print("Enter N to move NORTH")
-		print("Enter S to move SOUTH")
-		print("Enter E to move EAST")
-		print("Enter W to move WEST")
-		print("Enter M to return to the Main Menu")
-		print()
-
-	def GetMove(self):
-		Move = input()
-		print()
-		if Move != "":
-			return Move[0]
-		else:
-			return ""
-
-	def DisplayWonGameMessage(self):
-		print("Well done!  You have found the flask containing the Styxian potion.")
-		print("You have won the game of MONSTER!")
-		print()
-
-	def DisplayTrapMessage(self):
-		print("Oh no!  You have set off a trap.  Watch out, the monster is now awake!")
-		print()
-
-	def DisplayLostGameMessage(self):
-		print("ARGHHHHHHH!  The monster has eaten you.  GAME OVER.")
-		print("Maybe you will have better luck next time you play MONSTER!")
-		print()
-
-
-
-def DisplayMenu():
-	print("MAIN MENU")
-	print()
-	print("1.  Start new game")
-	print("2.  Play training game")
-	print("9.  Quit")
-	print()
-	print("Please enter your choice: ", end='')
-
-
-def GetMainMenuChoice():
-	Choice = int(input())
-	print()
-	return Choice
-
+	def Play(s):
+		try:
+			while True:
+				print(s.Cave)
+				s.Player.TakeTurn(s.Cave)
+				if len(s.Player.collectedItems) == len(s.items):
+					raise GameOverException(1)
+				s.Monster.TakeTurn(s.Player)
+		except GameOverException as winLoss:
+			if bool(winLoss.winLoss):
+				print("You won, congrats")
+			else:
+				print("Skill issue")
+		except KeyboardInterrupt:
+			pass
+	def Exit(s, *args):
+		print("You have chosen to exit")
+		raise Exit
+	def __init__(s):
+		options = {"0" : ("Play", s.Start, False),
+				   "1" : ("Play (Set Positions)", s.Start, True),
+				   "9" : ("Exit", s.Exit, "")}
+		while True:
+			print("Welcome to the Game, would you like to play")
+			print("\n".join([f"{x} : {options[x][0]}" for x in options]))
+			userInput = input("Input: ")
+			if userInput in options:
+				options[userInput][1](options[userInput][2])
+			else:
+				print("Invalid Input")
+		
 
 if __name__ == "__main__":
-	Choice = 0
-	while Choice != 9:
-		DisplayMenu()
-		Choice = GetMainMenuChoice()
-		if Choice == 1:
-			MyGame = Game(False)
-		elif Choice == 2:
-			MyGame = Game(True)
+	try:
+		while True:
+			Game()
+	except Exit:
+		pass
